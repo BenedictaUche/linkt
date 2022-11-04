1e43705d490ab5e91d578823a4dd57e434f68db7
Benedicta <benedictauche.onyebuchi@gmail.com>
2022-11-04 11:47:10 +0100

    Add: Removed some divs

---
+++ src/components/Contact.js
@@ -0,0 +1,47 @@
+const Contact = () => {
+  return (
+    <div className="form-box">
+      <form>
+        <div class="form-head">
+          <h2>Contact Me</h2>
+          <p>Hi there, contact me to ask about anything you have in mind</p>
+        </div>
+        <div class="row">
+          <div class="first-name" id="input-group">
+            <label htmlFor="first-name" className="">
+              First name
+            </label>
+            <input type="text" id="first_name" name="First-Name" placeholder="Enter your first name" />
+          </div>
+          <div class="last-name" id="input-group">
+            <label htmlFor="last-name" className="">
+              Last name
+            </label>
+            <input type="text" id="last_name" name="Last-Name" placeholder="Enter your last name" />
+          </div>
+        </div>
+        <div className="email-btn">
+          <label htmlFor="email" className="email">
+              Email
+          </label>
+          <input type="email" id="email" name="email" placeholder="yourname@email.com" />
+        </div>
+        <div className="message-btn">
+          <label htmlFor="message" className="">
+              Message
+          </label>
+          <textarea id="message" name="message" placeholder="Send me a message and i will reply you as soon as possible" />
+        </div>
+        <div class="radio-btn">
+          <input type="checkbox" id="radio" />
+          <label htmlFor="radio">You agree to provide your data to benedicta160 who may contact you</label>
+        </div>
+        <button type="submit" className="form-btn">
+          Send Message
+        </button>
+      </form>
+    </div>
+   );
+}
+ 
+export default Contact;
